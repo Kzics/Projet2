1ce00ec40ccf95bb6193e846e74eda8f48714dfb
Noslen Ul <nelson.uul@gmail.com>
2023-04-28 11:14:26 +0200

    MAJ tache 1 sans victoire

--- Tache1.py
+++ Tache1.py
@@ -1,3 +1,4 @@
+import random
 donjon = [[(False, True, True , False), (False, False, True, False)], [(True , True, False, True ), (True , True , True, False)]]
 position = (0,0)
 position1 = (1,1)
@@ -7,9 +8,10 @@ aventurier = {
     "position" : (0,1),
     "niveau" : 1
 }
-dragons = {
-    (1, 0): 3
-}
+dragons = [
+    {"position": (0,1), "niveau": 0},
+    {"position": (1,0), "niveau": 1}
+]
 
 def pivoter(donjon, position):
     rota_donjon = donjon[position[0]][position[1]][-1:] + donjon[position[0]][position[1]][:-1] #Créer une variable qui est l'addition du dernier indice de donjon et ajouter les éléments restants de ce dernier
@@ -42,35 +44,90 @@ def connecte(donjon, position1, position2):
             return False
 
 def intention(donjon, position, dragons):
+    # Fonction interne pour effectuer une recherche récursive dans le donjon
     def recherche(position, visite):
-        # Vérifie si la position actuelle est dans la liste des dragons
-        if position in dragons:
-            return [position], dragons[position]
-        # Vérifie si la position actuelle a déjà été visitée
+        # Vérifie si la position actuelle contient un dragon
+        for dragon in dragons:
+            if position == dragon['position']:
+                # Si oui, renvoie le chemin menant à cette position et le niveau du dragon
+                return [position], dragon['niveau']
+        # Si la position a déjà été visitée, renvoie None
         if position in visite:
             return None
+        # Ajoute la position actuelle aux positions visitées
         visite.add(position)
+        # Liste pour stocker les résultats des recherches récursives
         resultats = []
+        # Parcourt les positions voisines connectées à la position actuelle
         for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nouvelle_position = (position[0] + direction[0], position[1] + direction[1])
             if connecte(donjon, position, nouvelle_position):
+                # Effectue une recherche récursive à partir de la nouvelle position
                 resultat = recherche(nouvelle_position, visite)
                 if resultat is not None:
                     resultats.append(resultat)
-        # Vérifie si la liste des résultats est vide
+        # Si aucun résultat n'a été trouvé, renvoie None
         if not resultats:
             return None
-        chemin, niveau = max(resultats, key=lambda x: x[1])
+        # Sélectionne le résultat avec le niveau de dragon le plus élevé
+        niveau_max = max(resultats, key=lambda x: x[1])[1]
+        resultats = [resultat for resultat in resultats if resultat[1] == niveau_max]
+        chemin, niveau = random.choice(resultats)
+        # Renvoie le chemin menant à la position du dragon et son niveau
         return [position] + chemin, niveau
 
+    # Effectue une recherche récursive à partir de la position initiale de l'aventurier
     resultat = recherche(position, set())
-    # Vérifie si le résultat de l'appel à la fonction recherche est None
     if resultat is None:
         return None
     chemin, niveau = resultat
+    # Renvoie le chemin menant au dragon le plus proche avec le niveau le plus élevé
     return chemin
+
+def rencontre(aventurier, dragons):
+    # Récupère la position et le niveau de l'aventurier
+    position_aventurier = aventurier['position']
+    niveau_aventurier = aventurier['niveau']
+    # Parcourt la liste des dragons
+    for dragon in dragons:
+        # Récupère la position et le niveau du dragon
+        position_dragon = dragon['position']
+        niveau_dragon = dragon['niveau']
+        # Vérifie si le dragon se trouve à la même position que l'aventurier
+        if position_dragon == position_aventurier:
+            # Si oui, compare leurs niveaux
+            if niveau_dragon <= niveau_aventurier:
+                # Si le niveau du dragon est inférieur ou égal à celui de l'aventurier,
+                # le dragon est retiré de la liste et le niveau de l'aventurier est incrémenté de 1
+                aventurier['niveau'] += 1
+                dragons.remove(dragon)
+            else:
+                # Sinon, l'aventurier est considéré comme mort et la fonction renvoie False
+                return False
+    # Si l'aventurier survit à la rencontre avec les dragons, la fonction renvoie True
+    return True
+
+def appliquer_chemin(aventurier, dragons, chemin):
+    # Parcourt la liste des positions du chemin
+    for position in chemin:
+        # Met à jour la position de l'aventurier
+        aventurier['position'] = position
+        # Appelle la fonction rencontre pour vérifier si l'aventurier rencontre un dragon
+        rencontre(aventurier, dragons)
+
+def fin_partie(aventurier, dragons):
+    if not dragons:
+        # Si tous les dragons ont été tués, la partie est gagnée
+        return 1
+    elif 'vivant' in aventurier and not aventurier['vivant']:
+        # Si l'aventurier a été tué, la partie est perdue
+        return -1
+    else:
+        # Sinon, la partie continue
+        return 0
 
 print((donjon[1][1])) 
 print(pivoter(donjon, position))
 print(connecte(donjon, position1, position2))
 print(intention(donjon, position, dragons))
+print(rencontre(aventurier, dragons))
